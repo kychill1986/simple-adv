a1e00d605bf9118325bc957c3f4a2ce4588b2094
kychill1986 <yangchao20020@163.com>
2015-07-08 15:01:20 +0800

    update

--- src/main/java/com/yang/simple/web/SimpleController.java
+++ src/main/java/com/yang/simple/web/SimpleController.java
@@ -1,15 +1,15 @@
 package com.yang.simple.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.yang.simple.business.UserService;
 import com.yang.simple.redis.UserRepository;
 
-@RestController
+@Controller
 public class SimpleController {
 	
 	@Autowired
