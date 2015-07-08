a0b2a2d41294ec60481458601f4700ba5d8ae1fb
kychill1986 <yangchao20020@163.com>
2015-07-08 15:05:11 +0800

    udpate

--- src/main/java/com/yang/simple/web/SimpleController.java
+++ src/main/java/com/yang/simple/web/SimpleController.java
@@ -1,12 +1,16 @@
 package com.yang.simple.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
 import com.yang.simple.business.UserService;
+import com.yang.simple.entity.Users;
 import com.yang.simple.redis.UserRepository;
 
 @Controller
@@ -21,13 +25,14 @@ public class SimpleController {
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	@ResponseBody
 	public String hello() {
-		test();
-		return "hello world";
+		testRedis();
+		
+		List<Users> userList = userService.getAllUser();
+		Gson gson = new Gson();
+		return gson.toJson(userList);
 	}
 	
-	private void test(){
-		System.out.println(userService.getAllUser());
-		
+	private void testRedis(){
 		userRepository.saveUser("Chill");
 		System.out.println(userRepository.getUser());
 	}
